751c08d7c261c84418c348a3615d699fc8d2d8d1
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2019-03-31 19:58:47 +0200

    PlayerInputChannel now handles all types of inputs.

--- Returninator/Assets/Gameplay/Input/PlayerInputChannel.cs
+++ Returninator/Assets/Gameplay/Input/PlayerInputChannel.cs
@@ -8,14 +8,29 @@ namespace Returninator.Gameplay
     {
         private InputState m_LastInput = default;
 
+        private void UpdateAxis(InputAxis axis, Dictionary<InputAxis, float> values)
+        {
+            var inputValue = Input.GetAxis(axis.ToString());
+            if (Mathf.Abs(inputValue - m_LastInput[axis]) > 0.0001f)
+                values.Add(axis, inputValue);
+        }
+        private void UpdateAction(InputAction action, Dictionary<InputAction, bool> values)
+        {
+            var inputValue = Input.GetButton(action.ToString());
+            if (inputValue != m_LastInput[action])
+                values.Add(action, inputValue);
+        }
+
         public InputChange GetInputChange()
         {
             var axes = new Dictionary<InputAxis, float>();
+            UpdateAxis(InputAxis.Horizontal, axes);
+            UpdateAxis(InputAxis.Vertical, axes);
+            
             var actions = new Dictionary<InputAction, bool>();
-
-            var horizontal = Input.GetAxis("Horizontal");
-            if (Mathf.Abs(horizontal - m_LastInput.Horizontal) > 0.0001f)
-                axes.Add(InputAxis.Horizontal, horizontal);
+            UpdateAction(InputAction.Fire, actions);
+            UpdateAction(InputAction.Interact, actions);
+            UpdateAction(InputAction.Jump, actions);
 
             var change = new InputChange(actions, axes);
             m_LastInput.UpdateState(change);
