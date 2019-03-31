448657e82a3b074c8a73a0295dcc1a7f0cb83442
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2019-03-31 19:37:34 +0200

    Turned CharacterSettings into an asset. Turned each value into MovementParameters.

--- Returninator/Assets/Gameplay/Character/Character.cs
+++ Returninator/Assets/Gameplay/Character/Character.cs
@@ -7,32 +7,47 @@ using UnityEngine;
 namespace Returninator.Gameplay
 {
     [Serializable]
-    public class CharacterSettings
+    public struct MovementParameter
+    {
+        public MovementParameter(float value, AnimationCurve factor = null)
+        {
+            m_Value = value;
+            m_Factor = factor ?? AnimationCurve.Constant(0f, 1f, 1f);
+        }
+
+        [SerializeField]
+        private float m_Value;
+        [SerializeField]
+        private AnimationCurve m_Factor;
+
+        public float GetValue(float atTime)
+            => m_Value * m_Factor.Evaluate(atTime);
+    }
+
+    [CreateAssetMenu(fileName = "CharacterSettings", menuName = "Returninator/Character Settings")]
+    public class CharacterSettings: ScriptableObject
     {
         [SerializeField]
-        private float m_Acceleration = 10.0f;
+        private MovementParameter m_Acceleration = new MovementParameter(10f);
         [SerializeField]
-        private float m_Deacceleration = 5.0f;
+        private MovementParameter m_Deacceleration = new MovementParameter(5f);
+        [SerializeField]
+        private MovementParameter m_AirAcceleration = new MovementParameter(4f);
+        [SerializeField]
+        private MovementParameter m_AirDeacceleration = new MovementParameter(2f);
         [SerializeField]
         private float m_MaxSpeed = 10f;
 
-        [SerializeField]
-        private AnimationCurve m_AccelerationFactor = AnimationCurve.Constant(0, 1, 1f);
-        [SerializeField]
-        private AnimationCurve m_AirAccelerationFactor = AnimationCurve.Constant(0, 1, 1f);
-        [SerializeField]
-        private AnimationCurve m_DeaccelerationFactor = AnimationCurve.Constant(0, 1, 1f);
-        [SerializeField]
-        private AnimationCurve m_AirDeaccelerationFactor = AnimationCurve.Constant(0, 1, 1f);
-
-
         public float MaxAccelerationSpeed => m_MaxSpeed;
+        public float GetAcceleration(float speed, bool grounded)
+            => grounded ?
+                m_Acceleration.GetValue(speed) :
+                m_AirAcceleration.GetValue(speed);
 
-        public float GetAcceleration(float speed, bool grounded) => 
-            m_Acceleration * (grounded ? m_AccelerationFactor : m_AirAccelerationFactor).Evaluate(speed);
-
-        public float GetDeacceleration(float speed, bool grounded) =>
-            m_Deacceleration * (grounded ? m_DeaccelerationFactor : m_AirDeaccelerationFactor).Evaluate(speed);
+        public float GetDeacceleration(float speed, bool grounded)
+            => grounded ?
+                m_Deacceleration.GetValue(speed) :
+                m_AirDeacceleration.GetValue(speed);
     }
 
     [Serializable]
